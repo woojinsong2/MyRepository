717653f3a5ee0846c3e0a4437bae9d70e98ddd69
user <user@DESKTOP-V1DQ9AS>
2022-06-14 21:57:47 +0900

    Change in master after branching dev

--- HelloGit/src/MyClass.java
+++ HelloGit/src/MyClass.java
@@ -5,8 +5,8 @@ public class MyClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
 		System.out.println("This is ??? version.");
-		MyHello myHello=new MyHello();
-		myHello.sayHello();
+		System.out.println("Change in master branch");
+		
 	}
 
 }
