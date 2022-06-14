8d19a1f2aec6c79f827af4b15d4909c9565f4e40
user <user@DESKTOP-V1DQ9AS>
2022-06-14 23:08:16 +0900

    Changed A,B java by devB

--- MyGit Java/src/B.java
+++ MyGit Java/src/B.java
@@ -3,7 +3,7 @@ public class B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i am B");
+		System.out.println("i am changed B from devB");
 	}
 
 }
